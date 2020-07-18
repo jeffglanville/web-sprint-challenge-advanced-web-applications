2007809c46a34dbabcfdf949931770c55762444a
Jeff Glanville <jeff.glanville@outlook.com>
2020-07-18 12:16:07 -0500

    add logoff, private route and update app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import ProtectedRoute from './components/PrivateRoute';
+import BubblePage from "./components/BubblePage";
 import Login from "./components/Login";
+import Logoff from './components/Logoff';
 import "./styles.scss";
+
 
 function App() {
   return (
     <Router>
+      <nav className="nav">
+        <Link to="/logoff">Logoff</Link>
+      </nav>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute path="/protected" component={BubblePage} />
+        <ProtectedRoute path="/logoff" component={Logoff} />
       </div>
     </Router>
   );
