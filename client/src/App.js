eac4f6cc53d7cd8d7fb682c3a8af44da46615422
Jeff Glanville <jeff.glanville@outlook.com>
2020-07-18 13:12:57 -0500

    add contact page, update questions

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import ProtectedRoute from './components/PrivateRoute';
 import BubblePage from "./components/BubblePage";
 import Login from "./components/Login";
 import Logoff from './components/Logoff';
+import ContactPage from './components/ContactPage';
 import "./styles.scss";
+
 
 
 function App() {
@@ -13,10 +15,12 @@ function App() {
     <Router>
       <nav className="nav">
         <Link to="/logoff"><button className="logoff">Logoff</button></Link>
+        <Link to="/contactpage"><button>Click Here to enter your details</button></Link>
       </nav>
       <div className="App">
         <Route exact path="/" component={Login} />
         <PrivateRoute path="/protected" component={BubblePage} />
+        <PrivateRoute path="/contactpage" component={ContactPage} />
         <ProtectedRoute path="/logoff" component={Logoff} />
       </div>
     </Router>
